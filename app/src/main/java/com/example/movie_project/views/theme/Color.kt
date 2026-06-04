4d15bbbda8d7d5672ec9090fcd0aad3e48cb8601
Darnell O <Otto.Darnell@gmail.com>
2026-06-04 00:28:50 -0400

    so colors aren't duplicated/hardcoded inside the screen

---
+++ app/src/main/java/com/example/movie_project/views/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.movie_project.views.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Compose mirror of the brand colors defined in res/values/colors.xml,
+ * kept in one place so screens don't hardcode hex values.
+ */
+val Iris = Color(0xFF5D3FD3)
+val LightWhite = Color(0xFFFCFCFF)
+val DarkGrey = Color(0xFF979797)
