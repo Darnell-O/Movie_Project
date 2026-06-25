b84d47512b786ab4b83c941d1fa55343353f2168
Darnell O <Otto.Darnell@gmail.com>
2026-06-25 19:23:58 -0400

    update networking

--- app/src/main/java/com/example/movie_project/networking/MovieService.kt
+++ app/src/main/java/com/example/movie_project/networking/MovieService.kt
@@ -1,6 +1,6 @@
 package com.example.movie_project.networking
 
-import com.example.movie_project.models.MovieResponse
+import com.example.movie_project.models.dto.MovieResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
