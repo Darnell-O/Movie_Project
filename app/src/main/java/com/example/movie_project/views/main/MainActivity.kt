8dc2bd407a3b0bf684c135986410a3766ac80331
Darnell O <Otto.Darnell@gmail.com>
2026-07-08 16:57:33 -0400

    Navigation & auth flow

--- app/src/main/java/com/example/movie_project/views/main/MainActivity.kt
+++ app/src/main/java/com/example/movie_project/views/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.movie_project.views.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.movie_project.views.navigation.AppNavHost
 import com.example.movie_project.views.theme.MovieMagicTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             MovieMagicTheme {
